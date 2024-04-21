217e65d6ab99aa4a99d8514eb0d51a4291919624
vickypandey14 <vickypandeyts@gmail.com>
2024-04-21 19:09:18 +0530

    created two new api's

--- database.py
+++ database.py
@@ -2,10 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = ''
+DB_NAME = 'pysqldb'
+URL_DATABASE = f'mysql+pymysql://root@localhost/{DB_NAME}'
 
 engine = create_engine(URL_DATABASE)
 
-sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
---
+++ main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, HTTPException, Depends, status
+from pydantic import BaseModel
+from typing import Annotated
+import models
+from database import engine, sessionLocal
+from sqlalchemy.orm import Session
+
+app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    user_id: int
+    
+class UserBase(BaseModel):
+    username: str
+    
+    
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+   
+        
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.post("/users/", status_code=status.HTTP_201_CREATED)
+async def create_user(user: UserBase, db: db_dependency):
+    db_user = models.User(**user.model_dump())
+    db.add(db_user)
+    db.commit()
+    
+@app.get("/users/{user_id}", status_code=status.HTTP_200_OK)
+async def read_user(user_id: int, db: db_dependency):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    return user
